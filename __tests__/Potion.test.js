4a1488d98f130883e4da2bd6ad16352cd25d509d
Your Name <kalaitzidispaul@gmail.com>
2022-03-21 21:49:29 -0400

    added player function and mock directory

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -4,13 +4,13 @@ test('creates a health potion object', () => {
     const potion = new Potion('health');
 
     expect(potion.name).toBe('health')
-    expect(potion.value).toBe(expect.any(Number))
+    expect(potion.value).toEqual(expect.any(Number))
 })
 
 test('creates a random potion object', () => {
     const potion = new Potion();
 
     expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).tobeGreaterThan(0);
+    expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number))
 })
